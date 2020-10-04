8ae4e0cf360ebd9ed5245ae9c3fd13044b93c3a6
zanderhavgaard <zander@havgaard.dk>
2020-10-04 03:55:20 +0200

    made the terraform static ip not destroyable

--- terraform/floating_ip.tf
+++ terraform/floating_ip.tf
@@ -1,12 +1,11 @@
 resource "digitalocean_floating_ip" "static-droplet-ip" {
   region = "fra1"
-  # lifecycle {
-    # prevent_destroy = true
-  # }
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "digitalocean_floating_ip_assignment" "public-ip" {
   ip_address = digitalocean_floating_ip.static-droplet-ip.ip_address
   droplet_id = digitalocean_droplet.droplet.id
 }
-
